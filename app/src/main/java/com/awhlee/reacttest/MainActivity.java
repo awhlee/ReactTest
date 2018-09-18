c6533164bf0cce4c7d784c1cea5b02b0d410894a
Anthony Lee <awhlee@gmail.com>
2018-09-18 07:46:59 -0700

    First Commit

---
+++ app/src/main/java/com/awhlee/reacttest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.awhlee.reacttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
